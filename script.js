c302199f812e09553dc5800684cfb84b6cba20e5
meraj-sheikh <meraj.sheikh.1997@gmail.com>
2022-12-13 20:01:20 +0530

    Fixed mobile scroll

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector("#container"),
     smooth: true,
+    smartphone: {
+        smooth: true,
+        multiplier: 1.5
+    },
+    tablet: {
+        smooth: true,
+        multiplier: 1.5
+    }
 });
