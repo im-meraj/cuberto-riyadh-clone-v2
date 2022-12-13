e0e5e7758fd506e21bd96058a7a4e9e2f1f4e979
meraj-sheikh <meraj.sheikh.1997@gmail.com>
2022-12-13 18:14:01 +0530

    fixed mobile version glitches

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-const imgDiv = document.querySelector("#s-two-img-container");
-const imgDiv2 = document.querySelector("#s-four-img-container");
-
-const ltImg = document.querySelector("#s-six-left-top-img");
-const lbImg = document.querySelector("#s-six-left-bottom-img");
-const rtImg = document.querySelector("#s-six-right-top-img");
-const rbImg = document.querySelector("#s-six-right-bottom-img");
-
-// window.addEventListener("scroll", function() {
-//     console.log(window.scrollY);
-
-//     let offsetY = window.scrollY;
-
-//     imgDiv.style.backgroundPositionY = `${offsetY * 0.3}px`;
-//     imgDiv2.style.backgroundPositionY = `${offsetY * 0.3}px`;
-//     ltImg.style.backgroundPositionY = `${offsetY * 0.2}px`;
-//     rtImg.style.backgroundPositionY = `${offsetY * 0.2}px`;
-//     lbImg.style.backgroundPositionY = `${offsetY * 0.2}px`;
-//     rbImg.style.backgroundPositionY = `${offsetY * 0.5}px`;
-// })
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector("#container"),
     smooth: true,
-    mobile: true,
-    tablet: true,
-    smartphone: {
-        smooth: true
-    }
 });
